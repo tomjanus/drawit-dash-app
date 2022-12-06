85149679d74f5110e7011229f7564fd397bf9eeb
tjanus <tomasz.k.janus@gmail.com>
2022-12-06 02:11:28 +0100

    chore: make app.py compatible with

--- app.py
+++ app.py
@@ -18,15 +18,17 @@ DATA_PATH = os.path.join('data/', 'outputs_all_nondom.csv')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+data = load_outputs(DATA_PATH)
+app = dash.Dash(__name__) # , external_stylesheets=external_stylesheets
+server = app.server
 
-def main() -> None:
+
+def main(app, data) -> None:
     """ """
-    data = load_outputs(DATA_PATH)
-    app = dash.Dash(__name__) # , external_stylesheets=external_stylesheets
     app.title = 'Multicriteria land cover design results'
     create_layout(app, data)
-    app.run_server(debug=True)
+    app.run_server(debug=False)
 
 
 if __name__ == '__main__':
-    main()
+    main(app, data)
